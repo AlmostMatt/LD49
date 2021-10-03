27632a82e724a0b1f64bb6bb529c3515e714d647
Matthew Hyndman <myndman@gmail.com>
2021-10-03 12:36:13 -0400

    Add NavMesh prefab to some scenes
    The mesh can either be baked in the editor or a script I added can generate it at runtime.

---
+++ LD49/Assets/Scripts/NavMeshGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// This is a script so that I can be lazy about not baking navmesh in the editor
+public class NavMeshGenerator : MonoBehaviour
+{
+    public bool generateAtRuntime = false;
+
+    void Start()
+    {
+        if (generateAtRuntime)
+        {
+            GetComponent<NavMeshSurface>().BuildNavMesh();
+        }
+    }
+}
