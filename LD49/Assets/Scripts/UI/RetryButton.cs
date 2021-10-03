d36672ea48533cef195444e6cd4dffd661bcd005
Matthew Hyndman <myndman@gmail.com>
2021-10-03 14:34:34 -0400

    Add a retry button
    note - when reloading scenes in the editor lights go dark. this is apparently WAI due to how lights are baked and should not be an issue in actual builds
    https://answers.unity.com/questions/1264278/unity-5-directional-light-problem-please-help.html

---
+++ LD49/Assets/Scripts/UI/RetryButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryButton : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetLevel();
+        }
+    }
+
+    // Triggered by R or by reset button click
+    public void ResetLevel()
+    {
+        // TODO - generalize this if we have additive scene loading
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
+    }
+}
