c6bba073dd831be1c6dcde76da5874977cc7ea13
brendan-lojam <33971709+brendan-lojam@users.noreply.github.com>
2021-10-04 17:45:31 -0400

    bun heaven level

---
+++ LD49/Assets/Scripts/FinalLevelZoomTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLevelZoomTrigger : MonoBehaviour
+{
+    private bool mFired = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (mFired) return;
+
+        Debug.Log("final!");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("zoom!");
+            Camera camera = Camera.main;
+            CameraShake shake = camera.gameObject.GetComponent<CameraShake>();
+            StartCoroutine(shake.ZoomOut(2.5f, 1f));
+            mFired = true;
+
+            GameObject fx = GameObject.FindGameObjectWithTag("JoyfulEnvFx");
+            Transform toggle = fx.transform.GetChild(0);
+            toggle.gameObject.SetActive(true);
+            Transform child = toggle.Find("ParticleSystem");
+            child.localScale = new Vector3(1.5f, 1.5f, 1);
+            /*
+            ParticleSystem ps = child.GetComponent<ParticleSystem>();
+            ParticleSystem.ShapeModule shape = ps.shape;
+            shape.scale = Vector3.Scale(new Vector3(2.5f, 2.5f, 1f), shape.scale);
+            */
+        }
+    }
+}
